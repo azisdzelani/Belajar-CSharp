16dd57ae5b213467b5cf4582439b5455c417a624
Azis dzelani <azisdzelani@gmail.com>
2018-03-14 23:31:54 +0700

    test commit from visual studio

---
+++ BabArray/BabArray/Testing.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BabArray
+{
+    class Testing
+    {
+        static void bintang()
+        {
+            Console.WriteLine("a");
+            Console.ReadLine();
+        }
+    }
+}
